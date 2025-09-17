d11f34a2cbd927be2957017357aff70ed35f244b
Ahmed Ameen <amm00@fayoum.edu.eg>
2025-09-17 11:52:32 +0400

    تصحيح ملفات TypeScript و tsconfig للبناء على Vercel

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+apiKey: "AIzaSyBhRK9RJ7013-Au1X8_h0d1tuNwX2EQBGA",
+  authDomain: "test-ukf.firebaseapp.com",
+  projectId: "test-ukf",
+  storageBucket: "test-ukf.firebasestorage.app",
+  messagingSenderId: "686139152557",
+  appId: "1:686139152557:web:64e8a6eef2047cc2944e24",
+  measurementId: "G-J62FGQV9C9"
 };
 
 const app = initializeApp(firebaseConfig);
